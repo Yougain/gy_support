b7bbf532771316c7fba879a102afd39afa9eadf5
Yougain <from-github@you.dix.asia>
2026-03-09 17:25:58 +0900

    0.0.22  yougain@Fujitsu

--- %%dbv.zsh
+++ %%dbv.zsh
@@ -1,43 +1,81 @@
 
 
-	declare -A SCRIPT_FILES
+declare -A SCRIPT_FILES
 
 
-	emerge_file_content(){
-        local f="$1"
-		local sid=
-		if [ -e "$f" ];then
-			if [ -z "${SCRIPT_FILES[$f]}" ];then
-				sid=SCRIPT_FILE_$RANDOM$RANDOM$RANDOM
-				#dbv $sid
-				SCRIPT_FILES[$f]=$sid
-				local ln
-				eval "
-					$sid=()
-					local i=1
-					local IFS=""
-					while read -r ln;do
-						$sid+=(\"\$ln
-	\")
-						i=\$((i + 1))
-					done < $f
-				"
+emerge_file_content(){
+	local f="$1"
+	local sid=
+	if [ -e "$f" ];then
+		if [ -z "${SCRIPT_FILES[$f]}" ];then
+			sid=SCRIPT_FILE_$RANDOM$RANDOM$RANDOM
+			#dbv $sid
+			SCRIPT_FILES[$f]=$sid
+			local ln
+			eval "
+				$sid=()
+				local i=1
+				local IFS=""
+				while read -r ln;do
+					$sid+=(\"\$ln
+\")
+					i=\$((i + 1))
+				done < $f
+			"
+		else
+			sid=${SCRIPT_FILES[$f]}
+		fi
+	fi
+}
+
+
+
+source_depth() {
+	local -a ctx
+	local c depth=0
+	ctx=(${(s.:.)ZSH_EVAL_CONTEXT})
+
+	for c in $ctx; do
+		[[ $c == file ]] && ((depth++))
+	done
+
+	((depth--))          # 自分自身の file 分を引く
+	((depth < 0)) && depth=0
+	print -r -- "$depth"
+}
+
+
+preexec() {
+	if [ "$1" = "." -o "$1" = "source" ]; then
+		this_cmd="$1 $2"
+	else
+		this_cmd="$1"
+	fi
+}
+
+
+deb(){
+	if [ -n "$DEBUG" ];then
+		local w=
+		if [ "$DEBUG" = "l" -o "$DEBUG" = "L" ] && typeset -f require >/dev/null 2>&1;then
+			if require -q viewer; then
+				w=$(emerge_viewer)
 			else
-				sid=${SCRIPT_FILES[$f]}
+			    if [ -z "$VIEWER_FD" ]; then
+					mkdir -p /tmp/log
+			        exec {VIEWER_FD}>> "/tmp/log/$(basename $0).log"
+    			fi
+			    w=$VIEWER_FD				
 			fi
 		fi
-	}
-
-
-	function deb(){
-		if [ -n "$DEBUG" ];then
-			echo -en $debug
-			for ((i=0; i<${#BASH_SOURCE[@]}; i++)); do
-				echo -n " "
-			done		
-			echo -e $@$plain >&2
+		if [ -z "$w" ];then
+			w=2
 		fi
-	}
+		echo -en `date +"%Y-%m-%d %H:%M:%S.%3N"` $this_cmd\[$$\] $debug >&$w
+		printf '%*s' $(source_depth) '' >&$w
+		echo -e $@$plain >&$w
+	fi
+}
 
 
 
@@ -62,12 +100,12 @@ dbv(){
             ln="$(printf '%s' "$ln" | sed 's/[[:space:]]*$//')"
             if [[ "$ln" == *'`'* || "$ln" == *'$'* || "$ln" == *'(('* ]] ;then
                 ln="$(printf '%s' "$ln" | sed 's/[[:space:]]*$//')"
-                deb ${f##*/}:$rlno "'$yellow"${ln#*dbv }"$green'" = "'$cyan""$@""$green'"
+                deb ${f##*/}:$rlno ${fg[yellow]}${ln#*dbv }${fg[green]} = "'"${fg[cyan]}"$@"${fg[green]}"'"$plain
             else
-                deb ${f##*/}:$rlno $cyan$@
+                deb ${f##*/}:$rlno ${fg[cyan]}$@$plain
             fi
         else
-            deb ${f##*/}:$lno $cyan$@
+            deb ${f##*/}:$lno ${fg[cyan]}$@$plain
         fi
 	fi
 }
--- %local_path.sh
+++ %local_path.sh
@@ -1,16 +1,37 @@
-add_path_once() {
+insert_path_once() {
     local dir="$1"
     [ -d "$dir" ] || return 0
 
     case ":$PATH:" in
         *":$dir:"*)
             ;;              # 既に含まれている
-        *) PATH="$PATH:$dir"
+        *) PATH="$dir:$PATH"
             ;;     # 未登録なら追加（末尾）
     esac
 }
 
-add_path_once "$HOME/.local/bin"
-add_path_once "/usr/local/bin"
+insert_path_once "/usr/local/bin"
+insert_path_once "$HOME/.local/bin"
+
+remove_from_path() {
+  target="$1"
+  newpath=""
+  oldifs=$IFS
+  IFS=:
+  for p in $PATH; do
+    [ "$p" = "$target" ] && continue
+    if [ -z "$newpath" ]; then
+      newpath="$p"
+    else
+      newpath="$newpath:$p"
+    fi
+  done
+  IFS=$oldifs
+  PATH="$newpath"
+  export PATH
+}
+
+
+
 export PATH
 
